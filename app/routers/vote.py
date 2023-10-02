030f39394008d7d8ff4c49500592582e526b0463
ihkap11 <bpakhi97@gmail.com>
2023-10-02 13:07:46 +0100

    Bugfix: typo

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -22,7 +22,7 @@ def vote(
         if found_vote:
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
-                detail=f"user {current_user.id} has already voted on the post {vote.post.id}",
+                detail=f"user {current_user.id} has already voted on the post {vote.post_id}",
             )
         new_vote = models.Votes(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
