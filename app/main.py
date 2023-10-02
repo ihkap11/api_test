3cf532c9309c523129fb86b9d442c525a86d7a7e
ihkap11 <bpakhi97@gmail.com>
2023-10-02 11:53:13 +0100

    Add: vote route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -23,3 +23,4 @@ async def health():
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(auth.vote)
