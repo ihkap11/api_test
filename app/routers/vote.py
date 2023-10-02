bcaf7d121be54bba3c52ff8de7028e505f338d15
ihkap11 <bpakhi97@gmail.com>
2023-10-02 13:19:38 +0100

    Fix: datatype fixes

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/vote", tags=["Vote"])
 def vote(
     vote: schemas.Vote,
     db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.get_current_user),
+    current_user: UUID = Depends(oauth2.get_current_user),
 ):
     vote_query = db.query(models.Votes).filter(
         models.Votes.post_id == vote.post_id, models.Votes.user_id == current_user
@@ -22,9 +22,43 @@ def vote(
         if found_vote:
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
-                detail=f"user {current_user.id} has already voted on the post {vote.post_id}",
+                detail=f"user {current_user} has already voted on the post {vote.post_id}",
             )
-        new_vote = models.Votes(post_id=vote.post_id, user_id=current_user.id)
+        new_vote = models.Votes(post_id=vote.post_id, user_id=current_user)
+        db.add(new_vote)
+        db.commit()
+        db.refresh(new_vote)
+        return {"message": "Successfully added vote."}
+
+    else:
+        if not found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Vote doesn't exist."
+            )
+
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+
+        return {"message": "Successfully deleted vote."}
+
+
+@router.get("/votes", status_code=status.HTTP_201_CREATED)
+def read_vote(
+    vote: schemas.Vote,
+    db: Session = Depends(get_db),
+    current_user: UUID = Depends(oauth2.get_current_user),
+):
+    vote_query = db.query(models.Votes).filter(
+        models.Votes.post_id == vote.post_id, models.Votes.user_id == current_user
+    )
+    found_vote = vote_query.first()
+    if vote.vote_dir == 1:
+        if found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"user {current_user} has already voted on the post {vote.post_id}",
+            )
+        new_vote = models.Votes(post_id=vote.post_id, user_id=current_user)
         db.add(new_vote)
         db.commit()
         db.refresh(new_vote)
